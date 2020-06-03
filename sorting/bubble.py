cba301c125156e1b65f8513c686f8d4ad5243963
Pradeep <pradeep72c@gmail.com>
2020-06-03 13:02:22 +0530

    few changes

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -2,7 +2,7 @@ list1=[]
 num=int(input("enter how many numbers you want to enter?"))
 for k in range(num):
     list1.append(int(input()))
-# print(list1)
+print(list1)
 print("Unsorted list:", list1)
 for j in range(len(list1)-1):
     for i in range(len(list1)-1-j):
